f128944bcc9107eb19b5ff8eba18b31a482bcae8
Oluwanifemi-22 <nife22@stanford.edu>
2026-05-27 22:40:02 -0700

    fix build, add PWA manifest

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-import withPWA from "next-pwa";
+const nextConfig = {};
 
-const config = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-})({});
-
-export default config;
+export default nextConfig;
